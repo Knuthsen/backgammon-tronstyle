1cc0f0e127904b63af366bb21d76268d9f6bb277
Knuthsen <knuth.lampe@googlemail.com>
2026-07-31 01:48:09 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ const SOUNDS = {
 };
 
 // --- LAUTSTÄRKEN ANPASSEN (0.0 bis 1.0) ---
-SOUNDS.hit.volume = 0.2; // Halb so laut (50%)
+SOUNDS.hit.volume = 0.1; // Halb so laut (50%)
 SOUNDS.win.volume = 0.3; // Halb so laut (50%)
 
 function playSound(soundKey: keyof typeof SOUNDS) {
